abcffaa7b76816e67512f3c7bdb0d8a0ab52a5b3
aluno <aluno@ifpe.com>
2025-04-02 21:11:02 -0300

    criação e configuração do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodHillaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodHillaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodHillaryApplication.class, args);
+	}
+
+}
