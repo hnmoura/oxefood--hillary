7588a7fe5306f8dd6a5bae7df3647b896bab29e3
hnmoura <hlsnm@discente.ifpe.edu.br>
2025-07-09 09:38:53 -0300

    Corrigindo repository

--- src/main/java/br/com/ifpe/oxefood/modelo/acesso/UsuarioRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/acesso/UsuarioRepository.java
@@ -2,9 +2,10 @@ package br.com.ifpe.oxefood.modelo.acesso;
 
 import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
-   Optional<Usuario> findByUsername(String username);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
-
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
 }
